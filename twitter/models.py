a99475e60db09d7b79ce34fa4e46cda920f42074
Jayesh Gaba <jg9140@srmist.edu.in>
2024-10-12 01:33:29 +0530

    Profile pages

--- twitter/models.py
+++ twitter/models.py
@@ -10,12 +10,12 @@ class Profile(models.Model):
     date_modified = models.DateTimeField(User, auto_now=True)
     def __str__(self):
        return self.user.username
-   
+    def can_follow(self, profile_to_follow):
+        return self != profile_to_follow
+    
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
-        user_profile.save()
-        user_profile.follows.set([instance.profile.id])
         user_profile.save()
         
 post_save.connect(create_profile, sender=User)
--- twitter/views.py
+++ twitter/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib import messages
 from .models import Profile
 
@@ -10,5 +10,15 @@ def profile_list(request):
         profiles = Profile.objects.exclude(user=request.user)
         return render(request, 'profile_list.html', {"profiles":profiles})
     else:
-        messages.error(request, ("You must be logged in to view this page"))
-        return redirect('home')
+        return handle_unauthenticated_request(request)
+    
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = get_object_or_404(Profile, user_id=pk)  # Use get_object_or_404 for better error handling
+        return render(request, "profile.html", {"profile": profile})
+    else:
+        return handle_unauthenticated_request(request)
+
+def handle_unauthenticated_request(request):
+    messages.error(request, "You must be logged in to view this page.")
+    return redirect('home')
