465b52d8777338716f2d52c5648e81b72e0d232b
Jayesh Gaba <jg9140@srmist.edu.in>
2024-10-15 07:10:00 +0530

    basic implementation for profile image

--- twitter/models.py
+++ twitter/models.py
@@ -20,6 +20,8 @@ class Profile(models.Model):
     follows = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
     
     date_modified = models.DateTimeField(User, auto_now=True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    
     def __str__(self):
        return self.user.username
     def can_follow(self, profile_to_follow):
